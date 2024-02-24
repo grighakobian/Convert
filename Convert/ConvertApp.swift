7d4aed87e380674ec284c90ec596b79c8dbea402
Grigor Hakobyan <grighakobian@gmail.com>
2024-02-24 19:23:47 +0400

    Initial Commit

---
+++ Convert/ConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertApp.swift
+//  Convert
+//
+//  Created by Grigor Hakobyan on 24.02.24.
+//
+
+import SwiftUI
+
+@main
+struct ConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
