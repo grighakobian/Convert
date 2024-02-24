a7bbb763ef1ce2b764f3118cf94f6f328a9ab4de
Grigor Hakobyan <grighakobian@gmail.com>
2024-02-24 19:39:04 +0400

    feat: created app feature

---
+++ Convert/AppFeature/AppFeature.swift
@@ -0,0 +1,8 @@
+//
+//  AppFeature.swift
+//  Convert
+//
+//  Created by Grigor Hakobyan on 24.02.24.
+//
+
+import Foundation
---
+++ Convert/AppFeature/AppView.swift
@@ -0,0 +1,8 @@
+//
+//  AppView.swift
+//  Convert
+//
+//  Created by Grigor Hakobyan on 24.02.24.
+//
+
+import Foundation
